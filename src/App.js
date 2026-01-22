5813baefc815424c53bc8deff39d945a34d793f3
wookmin <alsdnr030226@naver.com>
2026-01-22 21:31:48 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -4,16 +4,13 @@ import {
   Routes,
   Route
 } from "react-router-dom";
-//import Home from "./routes/Home";
-//import Diary from "./routes/Diary";
-import StartPage from "./StartPage";
-import Login from "./Login";
-import SignUp from "./SignUp";
-//import GoogleLogin from "./components/loginPage";
+import Home from "./routes/Home";
+import Diary from "./routes/Diary";
+import StartPage from "./routes/StartPage";
+import Login from "./components/Login";
+import SignUp from "./routes/SignUp";
 import Loading from "./components/loading";
-import TestPage from "./components/TestPage";
 
-console.log({StartPage,Login,SignUp,Loading,TestPage});
 
 function App() {
   return (
@@ -23,7 +20,8 @@ function App() {
         <Route path="/Login" element={<Login/>} />
         <Route path="/SignUp" element={<SignUp/>}/>
         <Route path="/loading" element={<Loading />} />
-        <Route path="/Home" element={<TestPage />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/diary/:date" element={<Diary />} />
       </Routes>
     </Router>
   );
